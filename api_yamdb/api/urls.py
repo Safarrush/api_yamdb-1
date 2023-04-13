75d4a36d486a34dc4d26c56c67449f641be7ef84
Рушан Сафаргалеев <safargaleevrushan@yandex.ru>
2023-04-13 16:11:17 +0300

    Added users

---
+++ api_yamdb/api/urls.py
@@ -0,0 +1,14 @@
+from django.urls import include, path
+from rest_framework import routers
+
+from .views import UserViewSet
+
+app_name = 'api'
+
+router = routers.DefaultRouter()
+
+router.register('users', UserViewSet, basename='users')
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
